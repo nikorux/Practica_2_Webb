ca42dbd7c4206c5b9fb237f47ea78c3e0daba42c
Nikorux <nicole.0126@hotmail.es>
2019-12-02 19:00:14 -0400

    Commit

--- src/main/java/Estudiante.java
+++ src/main/java/Estudiante.java
@@ -4,4 +4,43 @@ public class Estudiante {
     private String apellido;
     private String telefono;
 
+    public Estudiante(int matricula, String nombre, String apellido, String telefono)
+    {
+        this.matricula = matricula;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.telefono = telefono;
+    }
+
+    public int getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(int matricula) {
+        this.matricula = matricula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
